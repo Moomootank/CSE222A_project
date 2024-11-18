ca71a391a56fa372cbddb42ecda5b4b2e130b621
CWJ <wchia@ucsd.edu>
2024-11-18 00:49:00 -0800

    Added useful commands

--- track_tcp_metrics.sh
+++ track_tcp_metrics.sh
@@ -10,14 +10,14 @@ KEY_FILE=$5
 echo "Monitoring ssthresh and cwnd for port $PORT..."
 echo "Press Ctrl+Z to stop."
 
-bash -c "while true; do ss -tin dport = :${PORT} >> ${OUTPUT_FILE_LOC}; sleep 0.1; done" & 
+bash -c "while true; do ss -tin dport = :${PORT} >> ${OUTPUT_FILE_LOC}.txt; sleep 0.1; done" & 
 ss_pid=$!
 
 
 # Start File download from server
 echo "Starting SFTP to fetch the file..."
 # sftp -i cse222a_key.pem "${URL}:${FILE_TO_DOWNLOAD}" "${FILE_TO_DOWNLOAD}"
-sftp -i ./cse222a_key.pem ${URL}:transferred/ <<< $"put ${FILE_TO_DOWNLOAD}"
+sftp -v -i ./cse222a_key.pem ${URL}:transferred/ 2> >(tee ${OUTPUT_FILE_LOC}-throughput.txt) <<< $"put ${FILE_TO_DOWNLOAD}"
 
 # Check if SFTP was successful
 if [ $? -eq 0 ]; then
