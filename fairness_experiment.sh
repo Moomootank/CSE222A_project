faf56e72a14bce73d532fee5479c9ce78eeefe5e
CWJ <wchia@ucsd.edu>
2024-12-08 15:04:34 -0800

    Final updates to scripts

--- fairness_experiment.sh
+++ fairness_experiment.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 URL=$1
 OUTPUT_FILE_LOC=$2
-NUM_FLOWS=$3
 
 echo "Starting fairness challenge"
 
+
 echo "Starting iperf3 to send data with main stream"
-iperf3 -c ${URL} -t 60 -P ${NUM_FLOWS} 2 >> ${OUTPUT_FILE_LOC}-main.txt
+iperf3 -c ${URL} -t 90 2 >> ${OUTPUT_FILE_LOC}-main.txt &
 
-#sleep 30
+sleep 30
 
-#echo "Starting iperf3 parallel streams"
-#iperf3 -c ${URL} -t 30  2 >> ${OUTPUT_FILE_LOC}-p1.txt &
-#iperf3 -c ${URL} -t 30  2 >> ${OUTPUT_FILE_LOC}-p2.txt &
+echo "Starting iperf3 parallel stream"
+iperf3 -c ${URL} -t 30 -p 50000 -P 2 2 >> ${OUTPUT_FILE_LOC}-parallel_1.txt &
+#iperf3 -c ${URL} -t 30 -p 50000 2 >> ${OUTPUT_FILE_LOC}-parallel_1.txt &
+#iperf3 -c ${URL} -t 30 -p 60000 2 >> ${OUTPUT_FILE_LOC}-parallel_2.txt &
 
 echo "Stopping fairness challenge..."
